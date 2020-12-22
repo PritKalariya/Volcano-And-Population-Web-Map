2c343f8f620982c0362a0771cefde6f31a161216
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-12-22 23:49:01 +0530

    Added a marker.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import folium
-from folium.map import Icon
 
 #Creating a basse map
 my_map = folium.Map(location = [38.58, -99.09], zoom_start = 5, tiles = "Stamen Terrain")
+
+#Adding a marker
+folium.Marker(location = [38.2, -99.1], popup = "Hii!! I'm a marker", icon = folium.Icon(color = "blue")).add_to(my_map)
 
 #Saving the map
 my_map.save("My Map.html")
