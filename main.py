b7928d9a761976599a946a9c724a576ca2705eb7
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-12-23 21:50:15 +0530

    Multiple pointers in the map.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import folium
 my_map = folium.Map(location = [38.58, -99.09], zoom_start = 5, tiles = "Stamen Terrain")
 
 #Adding a marker
-folium.Marker(location = [38.2, -99.1], popup = "Hii!! I'm a marker", icon = folium.Icon(color = "blue")).add_to(my_map)
+for coordinates in [[38.2, -99.1], [40.2, -96.1]]:
+    folium.Marker(location = coordinates, popup = "Hii!! I'm a marker", icon = folium.Icon(color = "blue")).add_to(my_map)
 
 #Saving the map
 my_map.save("My Map.html")
