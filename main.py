324a030dd9e59cd8d0feff3213da6c4f019d1356
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-12-28 21:57:32 +0530

    Loading data, and adding to pointers.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import folium
+import pandas as pd
+
+#Load the data
+data = pd.read_csv("Volcanoes.txt")
+
+#Create seperate lists from the data for better understanding
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 #Creating a basse map
 my_map = folium.Map(location = [38.58, -99.09], zoom_start = 5, tiles = "Stamen Terrain")
 
 #Adding a marker
-for coordinates in [[38.2, -99.1], [40.2, -96.1]]:
-    folium.Marker(location = coordinates, popup = "Hii!! I'm a marker", icon = folium.Icon(color = "blue")).add_to(my_map)
+for lt, ln, el in zip(lat, lon, elev):
+    folium.Marker(location = [lt, ln], popup = str(el) + " m", icon = folium.Icon(color = "blue")).add_to(my_map)
 
 #Saving the map
 my_map.save("My Map.html")
