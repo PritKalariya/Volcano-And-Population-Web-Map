fda1fc485e461b2ed782e907098d0513fb048f02
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-12-28 22:22:19 +0530

    Diff colors for diff vol.

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ Volcano name:<br>
 Height: %s m
 """
 
+#Function to define Icon color
+def color_generator(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
 #Creating a base map
 my_map = folium.Map(location = [38.58, -99.09], zoom_start = 5, tiles = "Stamen Terrain")
 
@@ -25,8 +34,8 @@ fg = folium.FeatureGroup(name = "My Map")
 #Adding a marker to the fg
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "red")))
+
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = color_generator(el))))
 
 my_map.add_child(fg)
 
