27a32029d5aa1766686aae47653448b6f074635f
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-12-28 22:06:34 +0530

    Formatting popup.

--- main.py
+++ main.py
@@ -8,13 +8,27 @@ data = pd.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+name = list(data["NAME"])
 
-#Creating a basse map
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+
+#Creating a base map
 my_map = folium.Map(location = [38.58, -99.09], zoom_start = 5, tiles = "Stamen Terrain")
 
-#Adding a marker
-for lt, ln, el in zip(lat, lon, elev):
-    folium.Marker(location = [lt, ln], popup = str(el) + " m", icon = folium.Icon(color = "blue")).add_to(my_map)
+#Creating a feature group for better understanding
+fg = folium.FeatureGroup(name = "My Map")
+
+#Adding a marker to the fg
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "red")))
+
+my_map.add_child(fg)
 
 #Saving the map
 my_map.save("My Map.html")
